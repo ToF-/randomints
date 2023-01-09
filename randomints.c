8b43e2597eef494133521b44ee93ec4e14bfa654
ToF- <cthibauttof@gmail.com>
2023-01-09 16:22:47 +0100

    added args

--- randomints.c
+++ randomints.c
@@ -8,10 +8,21 @@
 #include <assert.h>
 
 int main(int argc, char *argv[]) {
-    int range = 10000;
+    char c;
+    int start = 0;
+    int end = 10000;
+    while((c = getopt(argc, argv, "s:e:h")) != -1) {
+        switch(c) {
+            case 'h': printf("randomints -s <min> -e <max>\n");
+                      exit(0);
+            case 's': start = atoi(optarg); break;
+            case 'e': end = atoi(optarg); break;
+        }
+    }
+    int range = end - start;
     srand(time(NULL));
     while(true) {
-        int n = rand() % range;
+        int n = rand() % range + start;
         printf("%d\n", n);
     }
 }
